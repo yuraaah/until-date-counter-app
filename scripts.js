dfcfc247127f281eb42882350b4ea65d8ae6f43d
Yura <42500804+yuraaah@users.noreply.github.com>
2022-06-09 00:47:59 +0200

    Quick start
    Somewhat working default date (manual imput)
    
    Now I need to make it to accept custom dates

---
+++ scripts.js
@@ -0,0 +1,44 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
+
+const defaultDate = "1 Jan 2023";
+
+// -----------------
+document.getElementById('onClickDate').onclick = function dateInput() {
+    var customDate = document.getElementById("dateInputFrm").value;
+    countdown(customDate)
+}     
+
+// countdownd
+function countdown() {
+
+    const selectedDate = new Date(defaultDate);
+    const currentDate = new Date();
+
+    const totalSeconds = (selectedDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+// initial call
+countdown();
+
+setInterval(countdown, 1000);
+
+
+
+
