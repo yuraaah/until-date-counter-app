2c090aefbb3220219ca1e6f4fa9dd776b4e77859
Yura <42500804+yuraaah@users.noreply.github.com>
2022-06-09 23:31:21 +0200

    Semi done.

--- scripts.js
+++ scripts.js
@@ -3,32 +3,13 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const defaultDate = "1 Jan 2023";
+// countdown
+function countdown() {
+    var customDate = document.getElementById("dateInputFrm").valueAsNumber;
 
-// -----------------
-document.getElementById('onClickDate').onclick = function dateInput() {
-    var customDateMl = document.getElementById("dateInputFrm").value;
-    var customDate = new Date(customDateMl).toDateString();
-    
-    const year = customDate.substring(11,17);
-    const day = customDate.substring(8,10).replaceAll('0', '');
-    const month = customDate.substring(4,7);
-
-    // const customDatePass = (day + ' ' + month + ' ' + year);
-
-    console.log(day, month, year);
-    return(day, year, month);
-}  
-
-// Sun Jan 01 2023
-
-// countdownd
-function countdown(day, year, month) {
-
-    const selectedDate = new Date(defaultDate);
+    const selectedDate = customDate; 
     console.log(selectedDate)
     const currentDate = new Date();
-    console.log(currentDate)
 
     const totalSeconds = (selectedDate - currentDate) / 1000;
 
@@ -48,6 +29,7 @@ function formatTime(time) {
 }
 
 // initial call
+countdown();
 
 setInterval(countdown, 1000);
 
